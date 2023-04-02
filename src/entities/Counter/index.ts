b09893cf7adaf502712ecf25feef478d21688607
Stanislav <st.mele.a@yandex.ru>
2023-04-02 13:29:54 +0700

    fix export file, api

--- src/entities/Counter/index.ts
+++ src/entities/Counter/index.ts
@@ -1,3 +1,3 @@
 export { Counter } from './ui/Counter';
-export { CounterSchema } from './model/types/counterSchema';
+export { CounterSchema } from './model/types/CounterSchema';
 export { counterReducer } from './model/slice/counterSlice';
