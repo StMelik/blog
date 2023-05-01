db4235ac67e8d36643d93a9ed89ac3e02660e25c
Stanislav <st.mele.a@yandex.ru>
2023-05-01 11:24:40 +0700

    fix drawer, add browserslistrc

--- src/features/NotificationButton/ui/NotificationButton/NotificationButton.tsx
+++ src/features/NotificationButton/ui/NotificationButton/NotificationButton.tsx
@@ -52,14 +52,12 @@ export const NotificationButton = memo((props: NotificationButtonProps) => {
       </BrowserView>
       <MobileView>
         {trigger}
-        <AnimationProvider>
-          <Drawer
-            isOpen={isOpen}
-            onClose={handleClose}
-          >
-            <NotificationList />
-          </Drawer>
-        </AnimationProvider>
+        <Drawer
+          isOpen={isOpen}
+          onClose={handleClose}
+        >
+          <NotificationList />
+        </Drawer>
       </MobileView>
     </>
   );
--- src/shared/ui/Drawer/Drawer.tsx
+++ src/shared/ui/Drawer/Drawer.tsx
@@ -3,7 +3,7 @@ import {
   memo, ReactNode, useCallback, useEffect
 } from 'react';
 import { classNames } from '@/shared/lib/classNames/classNames';
-import { useAnimationLibs } from '@/shared/lib/components/AnimationProvider';
+import { AnimationProvider, useAnimationLibs } from '@/shared/lib/components/AnimationProvider';
 import { Overlay } from '../Overlay/Overlay';
 import { Portal } from '../Portal/Portal';
 import cls from './Drawer.module.scss';
@@ -99,7 +99,7 @@ export const DrawerContent = memo((props: DrawerProps) => {
   );
 });
 
-export const Drawer = memo((props: DrawerProps) => {
+export const DrawerAsync = (props: DrawerProps) => {
   const { isLoaded } = useAnimationLibs();
 
   if (!isLoaded) {
@@ -107,4 +107,10 @@ export const Drawer = memo((props: DrawerProps) => {
   }
 
   return <DrawerContent {...props} />;
-});
+};
+
+export const Drawer = (props: DrawerProps) => (
+  <AnimationProvider>
+    <DrawerAsync {...props} />
+  </AnimationProvider>
+);
