ee64eb78ac5e794474bff1279b6be5fa885a0370
Stanislav <st.mele.a@yandex.ru>
2023-05-08 17:23:52 +0700

    add appImage, fallback, errorFallback

--- src/entities/Article/ui/ArticleListItem/ArticleListItem.tsx
+++ src/entities/Article/ui/ArticleListItem/ArticleListItem.tsx
@@ -15,6 +15,8 @@ import {
 import { ArticleTextBlockComponent } from '../ArticleTextBlockComponent/ArticleTextBlockComponent';
 import cls from './ArticleListItem.module.scss';
 import { getRouteArticleDetails } from '@/shared/constants/router';
+import { AppImage } from '@/shared/ui/AppImage';
+import { Skeleton } from '@/shared/ui/Skeleton';
 
 interface ArticleListItemProps {
   className?: string;
@@ -42,14 +44,6 @@ export const ArticleListItem = memo((props: ArticleListItemProps) => {
     </>
   );
 
-  const image = (
-    <img
-      className={cls.img}
-      src={article.img}
-      alt={article.title}
-    />
-  );
-
   const date = <Text className={cls.date} text={article.createdAt} />;
 
   const title = <Text className={cls.title} title={article.title} />;
@@ -67,7 +61,12 @@ export const ArticleListItem = memo((props: ArticleListItemProps) => {
           </div>
           {title}
           {types}
-          {image}
+          <AppImage
+            fallback={<Skeleton width="100%" height={250} />}
+            className={cls.img}
+            src={article.img}
+            alt={article.title}
+          />
           {textBlock && <ArticleTextBlockComponent className={cls.textBlock} block={textBlock} />}
           <div className={cls.footer}>
             <AppLink target={target} to={getRouteArticleDetails(article.id)}>
@@ -90,7 +89,12 @@ export const ArticleListItem = memo((props: ArticleListItemProps) => {
     >
       <Card className={cls.card}>
         <div className={cls.imageWrapper}>
-          {image}
+          <AppImage
+            fallback={<Skeleton width={200} height={200} />}
+            className={cls.img}
+            src={article.img}
+            alt={article.title}
+          />
           {date}
         </div>
         <div className={cls.infoWrapper}>
